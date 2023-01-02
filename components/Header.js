97b4cbae7e2f053e15407d22eb131c8b28313ea3
Sarfaraz <102010896+sarfarazdev@users.noreply.github.com>
2023-01-02 19:26:30 +0530

    header ui

--- components/Header.js
+++ components/Header.js
@@ -75,7 +75,7 @@ function Header() {
     </div>
    
   
-    <div className="flex flex-row mt-2 items-center space-x-5 lg:mr-8 lg:mb-0 mb-3">
+    <div className="flex flex-row mt-2 items-center space-x-2 md:space-x-5 lg:space-x-5 lg:mr-8 lg:mb-0 mb-3">
                         <div className="relative flex items-center w-[200px] h-10 rounded-full overflow-hidden">
                             <div className="grid place-items-center h-full w-12 text-gray-300 hover:bg-slate-300 bg-slate-100">
                                 <svg aria-hidden="true" className="" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="#000" strokeWidth="1.5" d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853"></path></svg>
@@ -88,7 +88,7 @@ function Header() {
                                 placeholder="Search" />
                         </div>
 
-                        <div className='flex flex-row lg:space-x-5 space-x-2'>
+                        <div className='flex flex-row lg:space-x-5 md:space-x-5 space-x-2'>
                             <p>
                                 <svg aria-hidden="true" className="pre-nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="#000" strokeWidth="1.5" d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"></path></svg>
                             </p>
